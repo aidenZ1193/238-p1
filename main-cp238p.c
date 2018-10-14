c48737ba864e90c810fd37c1debf43674dd56b41
aidenZ1193 <aidenz1193@gmail.com>
2018-10-14 12:51:06 -0700

    Editted cp to support copy to dir

--- main-cp238p.c
+++ main-cp238p.c
@@ -28,9 +28,40 @@ main(int argc, char *argv[])
     }
 
     // 3. open file2
-    if((fd2 = open(argv[2], O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO)) == -1){
-        fprintf(stderr, "Source File open failed.\n");
-        exit(1);
+    // if file2 is s dir
+    struct stat file_stat;
+    stat(argv[2], &file_stat);
+    // if file is dir:
+    if(S_ISDIR(file_stat.st_mode) != 0){
+        int len1 = strlen(argv[1]);
+        int len2 = strlen(argv[2]);
+        // check if argv[2] contain '/'
+        if(argv[2][len2-1] == '/'){
+            // just add name to the end of argv[2]
+            char file2[len2+len1];
+            strncpy(file2, argv[2], len2);
+            strncpy(file2+len2, argv[1], len1);
+            if((fd2 = open(file2, O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO)) == -1){
+                fprintf(stderr, "Source File open failed.\n");
+                exit(1);
+            }
+        }else{
+            char file2[len2+len1+1];
+            strncpy(file2, argv[2], len2);
+
+            strncpy(file2+len2, "/", 1);
+            strncpy(file2+len2+1, argv[1], len1);
+            if((fd2 = open(file2, O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO)) == -1){
+                fprintf(stderr, "Source File open failed.\n");
+                exit(1);
+            }
+        }
+    }else{
+        // argv[2] not dirs
+        if((fd2 = open(argv[2], O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO)) == -1){
+            fprintf(stderr, "Source File open failed.\n");
+            exit(1);
+        }
     }
 
     char cp_buf[MAX_SIZE] = {};
